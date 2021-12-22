2a0b511551669bccd80c1c040e3018795e4f5e52
boenfu <1997@boenfu.cn>
2021-12-22 17:22:21 +0800

    修正 action 发布

--- publish.js
+++ publish.js
@@ -1,19 +1,14 @@
 // @ts-nocheck
 
 const {exec} = require('child_process');
-const fs = require('fs');
 
-const DIGSHARE_API = 'https://api.dingshao.cn/v1';
-const DIGSHARE_REGISTRY_URL = 'https://script.dingshao.cn/registry/';
+const DIGSHARE_API = 'https://api.staging.dingshao.cn/v1';
+const DIGSHARE_REGISTRY = 'https://script.staging.dingshao.cn/registry/';
 
-if (process.env['DIGSHARE_REGISTRY']) {
-  fs.writeFileSync('.npmrc', process.env['DIGSHARE_REGISTRY']);
-}
-
-let child = exec(`npm publish --registry=${DIGSHARE_REGISTRY_URL}`, {
+let child = exec(`npm publish --registry=${DIGSHARE_REGISTRY}`, {
   env: {
     DIGSHARE_API,
-    DIGSHARE_REGISTRY_URL,
+    DIGSHARE_REGISTRY,
     ...process.env,
   },
 });
