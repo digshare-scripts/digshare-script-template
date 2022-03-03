15e768c027578bcdc55612b5d34ec1e35ec15ea4
boenfu <1997@boenfu.cn>
2022-03-03 09:54:34 +0800

    update sdk version

--- publish.js
+++ publish.js
@@ -2,8 +2,8 @@
 
 const {exec} = require('child_process');
 
-const DIGSHARE_API = 'https://api.staging.dingshao.cn/v1';
-const DIGSHARE_REGISTRY = 'https://script.staging.dingshao.cn/registry/';
+const DIGSHARE_API = 'https://api.dingshao.cn/v1';
+const DIGSHARE_REGISTRY = 'https://script.dingshao.cn/registry/';
 
 let child = exec(`npm publish --registry=${DIGSHARE_REGISTRY}`, {
   env: {
